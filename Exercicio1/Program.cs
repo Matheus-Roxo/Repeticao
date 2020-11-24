b38a29e85ea97b606bbbf9bd6a8cc796c2793c3b
Matheus Roxo Brandão <matheus.roxo.brandao31@gmail.com>
2020-11-24 09:28:07 -0300

    Exercicios feitos

---
+++ Exercicio1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Exercicio1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite a nota:");
+            int nota = int.Parse(Console.ReadLine());
+           
+            while(nota >= 10){
+            Console.WriteLine("Valor inválido, Digite o valor novamente");
+            Console.WriteLine("Digite a nota: ");
+            nota = int.Parse(Console.ReadLine());
+            }
+
+        }
+    }
+}
---
+++ Exercicio2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Exercicio2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite o usuário: ");
+            string nome = Console.ReadLine();
+
+            Console.WriteLine("Digite a senha: ");
+            string senha = Console.ReadLine();
+
+            while(nome == senha){
+            Console.WriteLine("A senha deve ser diferente do usuário, digite uma nova senha: ");
+            Console.WriteLine("Digite a sua senha: ");
+            senha = Console.ReadLine();
+            }
+            Console.WriteLine("Você foi cadastrado com sucesso!");
+        }
+    }
+}
